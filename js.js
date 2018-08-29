587f954178b932fb74dba7c367a91cf7ae510727
trainingitsdm <42791031+trainingitsdm@users.noreply.github.com>
2018-08-29 21:50:41 +0700

    Update js.js

--- js.js
+++ js.js
@@ -8,11 +8,12 @@ function scrZN(z) {
 }
 LinkTwitArr=["1033880969365712896","1031433658006007808","1031433299929919488","1033983001401511936"];
 if(LINK=="about:blank"){	
-	HTML = "<h3><center><a href=''>Reload</a></center></h3>";
+	HTML = "<html><body style='background:#2C789F'>";
 	LTwt = LinkTwitArr[Math.floor(Math.random()*(LinkTwitArr.length))];
 	u='https://mobile.twitter.com/Paket_Wisata_Ok/status/'+LTwt;
 	//u='https://medium.com/@arenawisataid/paket-wisata-jogja-murah-arena-wisata-f5eb743fae7c';
-	HTML+='<a class="start" href="'+u+'">Post: '+LTwt+'</a>';
+	HTML+='<a class="start" id='StartNow' href="'+u+'">Start Now</a>';
+	HTML+='</body></html>';
 	document.body.innerHTML = HTML;
 }else if(LINK.indexOf("medium.com")>-1){	
 	var L = document.querySelector("a[href*='paket-wisata-jogja.html']");
