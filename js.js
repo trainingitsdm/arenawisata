c954b0a48e7e0074041240b8a508afbb66ca47d5
trainingitsdm <42791031+trainingitsdm@users.noreply.github.com>
2018-08-29 09:22:54 +0700

    Update js.js

--- js.js
+++ js.js
@@ -52,4 +52,11 @@ if(LINK=="about:blank"){
 		},((100*Math.floor((Math.random() * 100) + 1))+60000));
 	}
 	
-}
+}else if(URL.indexOf("mobile.twitter")>-1){
+	function CekLink(No=0){
+		//G-Plus , Arena Wisata, Medium, Facebook
+		var A=document.querySelectorAll("[href*='PEwOo3WzW0'],[href*='TT11IHesZH'],[href*='vTyabc1YR2'],[href*='QkIZ5Ch6DJ']");
+		return A.length?A[0].click():(No<20?setTimeout(function(){CekLink(No+1)},1000):false);
+	}
+	CekLink();
+} 
