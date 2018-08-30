b4f3c4ff6d897d8a3c29f0354b871633dd787c63
trainingitsdm <42791031+trainingitsdm@users.noreply.github.com>
2018-08-30 16:20:18 +0700

    Update js.js

--- js.js
+++ js.js
@@ -20,7 +20,7 @@ if(LINK=="about:blank"){
 		+ '<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
 	HTML+='</body></html>';
 	document.body.innerHTML = HTML;
-}else document.body.innerHTML = LINK;
+}else document.body.innerHTML = 'document.location.href';
 if(LINK.indexOf("medium.com")>-1){	
 	var L = document.querySelector("a[href*='paket-wisata-jogja.html']");
 	L.removeAttribute("rel");
